d0223859395257680d9211197ee97f897dc990cf
RAHULGOLE123 <rahulgole1296@gmail.com>
2025-08-09 21:16:18 +0530

    implement dynamic title metadata

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,13 @@
+import { Metadata } from 'next'
 import React from 'react'
+import { deflate } from 'zlib'
+
+//contact page
+export const metadata: Metadata = {
+  title:{
+    absolute:'contact Next.js'
+  }
+}
 
 export default function page() {
   return (
